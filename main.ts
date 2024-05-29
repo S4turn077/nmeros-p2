b6ddaa0b5b778039271cae017b540f639d63f50b
S4turn077 <165195107+S4turn077@users.noreply.github.com>
2024-05-29 14:10:37 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+let a = 0
+let b = 0
+input.onButtonPressed(Button.A, function () {
+    a = randint(1, 12)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(a * b)
+})
+input.onButtonPressed(Button.B, function () {
+    b = randint(1, 12)
+})
